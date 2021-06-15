f78bef3be5dff4d25f5b2422ad77c31be9e207d1
Damon George <33606025+DamonGeorge@users.noreply.github.com>
2021-06-15 16:38:58 -0700

    Added search bar for project tags

--- src/scripts/projects.js
+++ src/scripts/projects.js
@@ -1,3 +1,5 @@
+/* eslint-disable strict */
+
 $(function () {
     initTags();
 });
@@ -7,11 +9,23 @@ function initTags() {
     var tagsMapping = {};
     var activeTags = [];
 
-    projects = $(".projectContainer");
+    // elements
+    var projects = $(".projectContainer");
+    var tagsContainer = $("#filteredTagsContainer");
+    var tagsList = $("#filteredTagsContainer ul");
+    var searchInput = $("#filterSearchInput");
+    var clearSearchIcon = $("#filterSearchClearIcon");
+    var noTagsMessage = $("#filteredTagsContainer #noTagsMessage");
+    var extraTagsContainer = $("#extraTagsContainer");
+    var extraTagsList = $("#extraTagsContainer ul");
+
+    extraTagsContainer.hide();
+
+    // find all tags and fill tagsMapping
     projects.each(function (i, el) {
-        tags = $(el).find(".projectHeader li");
+        var tags = $(el).find(".projectHeader li");
         tags.each(function (j, tagEl) {
-            tag = tagEl.textContent;
+            var tag = tagEl.textContent;
             if (tag in tags) {
                 tagsMapping[tag].push(i);
             } else {
@@ -20,56 +34,114 @@ function initTags() {
         })
     });
 
-    console.log(tagsMapping)
-
-    tagsList = $(".projectFilteringContainer ul");
-
-    validTags = Object.getOwnPropertyNames(tagsMapping);
+    // gen html for all tags
+    var validTags = Object.getOwnPropertyNames(tagsMapping);
     validTags.sort();
     validTags.forEach(function (t) {
         tagsList.append("<li>" + t + "<i class='material-icons'>close</i></li>");
+        extraTagsList.append("<li class='active hidden'>" + t + "<i class='material-icons'>close</i></li>");
     });
 
+    // lists of all the jquery list elements for the tags
+    var tagsListItems = tagsList.children("li");
+    var extraTagsListItems = extraTagsList.children("li");
 
-    tagsList.click(function (event) {
-        target = $(event.target);
+    // click handler for each tag in the main list
+    tagsContainer.click(function (event) {
+        var target = $(event.target);
         if (target.is("li")) {
-            clickedTag = target[0].childNodes[0].textContent;
+            var clickedTag = target[0].childNodes[0].textContent;
             if (target.hasClass("active")) {
                 activeTags.splice(activeTags.indexOf(clickedTag), 1);
             } else {
                 activeTags.push(clickedTag);
             }
             target.toggleClass("active");
-            refilter();
-            highlightTags();
+            filterProjects();
         }
     });
 
-    var refilter = function () {
+    // click handler for each tag in the extra list
+    extraTagsContainer.click(function (event) {
+        var target = $(event.target);
+        if (target.is("li")) {
+            var clickedTag = target[0].childNodes[0].textContent;
+            activeTags.splice(activeTags.indexOf(clickedTag), 1);
+            target.hide();
+            tagsListItems.eq(validTags.indexOf(clickedTag)).removeClass("active");
+            filterProjects();
+        }
+    });
+
+    //handler for search Input
+    searchInput.on("input", function (e) {
+        filterTags($(this).val().toLowerCase());
+
+    });
+
+    clearSearchIcon.click(function (e) {
+        searchInput.val("");
+        filterTags("");
+    });
+
+
+    var filterTags = function (search) {
+        var extra = false;
+        var found = false;
+        validTags.forEach(function (t, i) {
+            if (t.toLowerCase().indexOf(search) > -1) {
+                found = true;
+                // found
+                tagsListItems.eq(i).show();
+                extraTagsListItems.eq(i).hide();
+            } else {
+                // excluded
+                tagsListItems.eq(i).hide();
+                // if active show to extra list
+                if (activeTags.indexOf(t) > -1) {
+                    extraTagsListItems.eq(i).show();
+                    extra = true;
+                } else {
+                    extraTagsListItems.eq(i).hide();
+                }
+            }
+        })
+        if (found) {
+            noTagsMessage.hide();
+        } else {
+            noTagsMessage.show();
+        }
+        if (extra) {
+            extraTagsContainer.show();
+        } else {
+            extraTagsContainer.hide();
+        }
+    }
+
+    var filterProjects = function () {
         if (activeTags.length) {
             projects.each(function (i, el) {
-                $(el).addClass("hidden");
+                $(el).hide();
             });
 
             activeTags.forEach(function (t) {
                 projIdxs = tagsMapping[t];
                 projIdxs.forEach(function (i) {
-                    $(projects[i]).removeClass("hidden");
-                    highlightTags($(projects[i]));
+                    $(projects[i]).show();
+                    highlightProjects($(projects[i]));
                 });
             });
         } else {
             projects.each(function (i, el) {
-                $(el).removeClass("hidden");
+                $(el).show();
             });
         }
     };
 
-    var highlightTags = function (projectContainer) {
+    var highlightProjects = function (projectContainer) {
         projects.each(function (i, el) {
             $(el).find(".projectHeader li").each(function (i, tagEl) {
-                t = tagEl.textContent;
+                var t = tagEl.textContent;
                 if (activeTags.includes(t)) {
                     $(tagEl).addClass("active");
                 } else {
